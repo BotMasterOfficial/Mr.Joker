4b0bbaea98e9e0da9871fad0a736f8da88655f3b
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-09-27 22:40:53 +0530

    Update errors.py

--- mrjoker/utils/errors.py
+++ mrjoker/utils/errors.py
@@ -4,7 +4,7 @@ from functools import wraps
 from mrjoker import pbot as app
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-LOG_GROUP_ID = int(-1001162243028)
+LOG_GROUP_ID = int(-1001594744091)
 
 def split_limits(text):
     if len(text) < 2048:
